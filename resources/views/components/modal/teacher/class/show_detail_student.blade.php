098370f3c82b13fb075f32b940bc11cd87d22099
sulistiyas <sulisgor.a@gmail.com>
2026-02-27 14:07:45 +0700

    update

---
+++ resources/views/components/modal/teacher/class/show_detail_student.blade.php
@@ -0,0 +1,21 @@
+    <div class="modal fade" id="modal_students">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Student List</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody id="students_body"></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
